bd68d94bfd5a2cb9d455fc18cdfadb7771b307f1
dmitrime <dmitrime@github.com>
2016-08-04 09:20:32 +0700

    improve max diff

--- code/max-diff.py
+++ code/max-diff.py
@@ -7,24 +7,39 @@ def max_diff1(a):
     return diff
 
 def max_diff2(a):
-    minimum, diff = a[0], 0
-    for i in range(1, len(a)):
-        if a[i] - minimum > diff:
-            diff = a[i] - minimum
-        if a[i] < minimum:
-            minimum = a[i]
+    diff = 0
+    if len(a) >= 2:
+        minimum = a[0]
+        for elem in a:
+            minimum = min(minimum, elem)
+            diff = max(diff, elem-minimum)
     return diff
 
 def max_diff3(a):
-    maximum, diff = a[-1], 0
-    for i in range(len(a)-2, -1, -1):
-        if maximum - a[i] > diff:
-            diff = maximum - a[i]
-        if maximum < a[i]:
-            maximum = a[i]
+    diff = 0
+    if len(a) >= 2:
+        maximum = a[-1]
+        for elem in reversed(a):
+            maximum = max(maximum, elem)
+            diff = max(diff, maximum-elem)
     return diff
 
 if __name__ == '__main__':
-    assert max_diff2([9, 3, 6, 8, 10, 4, 3]) == 7
+    assert max_diff1([]) == 0
+    assert max_diff1([1]) == 0
+    assert max_diff1([9, 3, 6, 8, 10, 4, 3]) == 7
     assert max_diff1(range(10+1)) == 10
     assert max_diff1(range(10, 0, -1)) == 0
+
+    assert max_diff2([]) == 0
+    assert max_diff2([1]) == 0
+    assert max_diff2([9, 3, 6, 8, 10, 4, 3]) == 7
+    assert max_diff2(range(10+1)) == 10
+    assert max_diff2(range(10, 0, -1)) == 0
+
+    assert max_diff3([]) == 0
+    assert max_diff3([1]) == 0
+    assert max_diff3([9, 3, 6, 8, 10, 4, 3]) == 7
+    assert max_diff3(range(10+1)) == 10
+    assert max_diff3(range(10, 0, -1)) == 0
+
